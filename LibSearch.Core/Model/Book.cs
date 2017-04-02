9e91ad6e30f50563145ee5c504268ae944dfd45a
Kirichenko-sanek <alexander.kirichenkoo@gmail.com>
2017-04-02 18:11:00 +0300

    add repository

--- LibSearch.Core/Model/Book.cs
+++ LibSearch.Core/Model/Book.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LibSearch.Core.Model
+﻿namespace LibSearch.Core.Model
 {
     public class Book : BaseEntity
     {
