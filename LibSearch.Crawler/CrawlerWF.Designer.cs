ca4959fd5b17d64fb6b20f78b5615ae0baee0285
Kirichenko-sanek <alexander.kirichenkoo@gmail.com>
2017-04-19 04:23:21 +0300

    edit ui crawler

--- LibSearch.Crawler/CrawlerWF.Designer.cs
+++ LibSearch.Crawler/CrawlerWF.Designer.cs
@@ -29,26 +29,42 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.progressBarResult = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(44, 75);
+            this.button1.Location = new System.Drawing.Point(16, 29);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(281, 44);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Start crawler";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBarResult
+            // 
+            this.progressBarResult.Location = new System.Drawing.Point(12, 153);
+            this.progressBarResult.Name = "progressBarResult";
+            this.progressBarResult.Size = new System.Drawing.Size(285, 38);
+            this.progressBarResult.TabIndex = 1;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
             // 
             // CrawlerWF
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(496, 261);
+            this.ClientSize = new System.Drawing.Size(309, 215);
+            this.Controls.Add(this.progressBarResult);
             this.Controls.Add(this.button1);
             this.Name = "CrawlerWF";
-            this.Text = "Form1";
+            this.Text = "Crawler";
             this.ResumeLayout(false);
 
         }
@@ -56,6 +72,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBarResult;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
     }
 }
 
--- LibSearch.Crawler/CrawlerWF.cs
+++ LibSearch.Crawler/CrawlerWF.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using LibSearch.Crawler.BL.Interfaces.Manager;
@@ -20,25 +21,52 @@ namespace LibSearch.Crawler
         public CrawlerWF(IInostrankabooksManager inostrankabooksManager)
         {
             InitializeComponent();
+            backgroundWorker.WorkerReportsProgress = true;
 
             _inostrankabooksManager = inostrankabooksManager;
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
         private void button1_Click(object sender, EventArgs e)
         {
-            _inostrankabooksManager.GetInfo(path);
+            button1.Enabled = false;
+            var a = new Thread(() =>
+            {
+                _inostrankabooksManager.GetInfo(path);
+            });
+            backgroundWorker.RunWorkerAsync();
+            a.Start();
+        }
+
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Thread.Sleep(10000);
+            for (;;)
+            {
+                var a = _inostrankabooksManager.GetProgressMax();
+                var b = _inostrankabooksManager.GetProgressNow();
+                if (a != 0 && b != 0)
+                {
+                    int c = b * 100 / a;
+                    backgroundWorker.ReportProgress(c);
+                }
+                Thread.Sleep(2000);
+                if (b >= a && a != 0)
+                {
+                    backgroundWorker.ReportProgress(0);
+                    break;
+                }
+            }
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBarResult.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            button1.Enabled = true;
         }
     }
 }
